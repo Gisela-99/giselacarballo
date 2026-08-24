ac1d2bb8b68a910f99f2fb1742bf1303c2bdb466
Gisela-99 <giselacarballour@gmail.com>
2026-08-25 01:35:25 +0200

    refactor: limpia componentes y páginas obsoletas

--- Portafolio/src/app/Router.tsx
+++ Portafolio/src/app/Router.tsx
@@ -2,19 +2,13 @@
 
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
-import Contacto from '../pages/Contacto/Contacto'
-import SobreMi from '../pages/SobreMi/SobreMi';
-import Proyectos from '../pages/Proyectos/Proyectos';
 import Home from '../pages/Home/Home';
 
 const Router = () => (
     <BrowserRouter>
     <Layout>
         <Routes>   
-            <Route index element={<Home/>} />
-            <Route path="about" element={<SobreMi/>} />
-            <Route path="projects" element={<Proyectos/>} />
-            <Route path="contact" element={<Contacto/>} />  
+            <Route index element={<Home/>} /> 
             <Route path="*" element={<div>404</div> } />
         </Routes>
     </Layout>
